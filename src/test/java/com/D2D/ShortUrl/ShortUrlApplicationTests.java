23aff4143466e72c14a8eccbc790722d52dfe581
ANTIC MARKO <[antic.marko69@gmail.com](mailto:antic.marko69@gmail.com)>
2023-07-21 13:19:34 +0200

    coucou

---
+++ src/test/java/com/D2D/ShortUrl/ShortUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.D2D.ShortUrl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
