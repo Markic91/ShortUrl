f515bc4ea6f6edf50474d6fd15e8f51ba5caff5e
ANTIC MARKO <[antic.marko69@gmail.com](mailto:antic.marko69@gmail.com)>
2023-07-21 13:48:42 +0200

    initialize SortUrl entity

--- src/main/java/com/D2D/ShortUrl/Entity/ShortUrl.java
+++ src/main/java/com/D2D/ShortUrl/Entity/ShortUrl.java
@@ -1,5 +1,37 @@
 package com.D2D.ShortUrl.Entity;
 
-public class ShortUrl {
+import java.net.URL;
 
+public class ShortUrl {
+    private String id;
+    private String shortId;
+    private URL realUrl;
+
+
+    public ShortUrl() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getShortId() {
+        return shortId;
+    }
+
+    public void setShortId(String shortId) {
+        this.shortId = shortId;
+    }
+
+    public URL getRealUrl() {
+        return realUrl;
+    }
+
+    public void setRealUrl(URL realUrl) {
+        this.realUrl = realUrl;
+    }
 }
