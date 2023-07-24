e2d8029ee0ed592e40fcc22b0496aea891189c7a
mcdmat <[matheyd@gmail.com](mailto:matheyd@gmail.com)>
2023-07-24 09:50:39 +0200

    re

--- src/main/java/com/D2D/ShortUrl/Controller/ShortUrlController.java
+++ src/main/java/com/D2D/ShortUrl/Controller/ShortUrlController.java
@@ -2,28 +2,23 @@ package com.D2D.ShortUrl.Controller;
 
 import com.D2D.ShortUrl.Entity.ShortUrl;
 import com.D2D.ShortUrl.Entity.Url;
-//import com.D2D.ShortUrl.Repository.ShortIdGenerator;
-import com.D2D.ShortUrl.Repository.UUIDGenerator;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.UUID;
 
 @RestController
 public class ShortUrlController {
-    @Autowired
-    UUIDGenerator uuidGenerator;
-//    @Autowired
-//    ShortIdGenerator shortIdGenerator;
+
 
     @PostMapping("/links")
     public ShortUrl createUrlObject (@RequestBody Url myNewUrl) throws MalformedURLException {
         ShortUrl ObjectToCreate = new ShortUrl();
-        ObjectToCreate.setId(uuidGenerator.getUuid().toString());
-//        ObjectToCreate.setShortId(shortIdGenerator.shortId());
+        ObjectToCreate.setId(UUID.randomUUID().toString());
+        ObjectToCreate.setShortId("coucou");
         ObjectToCreate.setRealUrl(new URL(myNewUrl.getMyUrl().toString()));
 
         return ObjectToCreate;
