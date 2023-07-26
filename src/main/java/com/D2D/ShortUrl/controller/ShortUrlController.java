3e8e71a7b8f3be8b10f82f9f825d6786824952e3
ANTIC MARKO <[antic.marko69@gmail.com](mailto:antic.marko69@gmail.com)>
2023-07-26 15:14:28 +0200

    coucou

--- src/main/java/com/D2D/ShortUrl/controller/ShortUrlController.java
+++ src/main/java/com/D2D/ShortUrl/controller/ShortUrlController.java
@@ -2,17 +2,16 @@ package com.D2D.ShortUrl.controller;
 
 import com.D2D.ShortUrl.entity.ShortUrl;
 import com.D2D.ShortUrl.service.ShortIdGenerator;
-import org.apache.tomcat.util.json.JSONParser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.web.bind.annotation.*;
 import com.D2D.ShortUrl.repository.SaveFile;
 import org.springframework.web.servlet.ModelAndView;
+
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.*;
-
 
 
 @RestController
@@ -22,19 +21,19 @@ public class ShortUrlController {
     private final ShortIdGenerator shortIdGenerator;
     private final SaveFile savefile;
 
-    public ShortUrlController(ShortIdGenerator shortIdGenerator, SaveFile saveFile){
+    public ShortUrlController(ShortIdGenerator shortIdGenerator, SaveFile saveFile) {
         this.shortIdGenerator = shortIdGenerator;
         this.savefile = saveFile;
     }
 
 
     @PostMapping("/links")
-    public ShortUrl createUrlObject (@RequestBody URL myNewUrl) throws MalformedURLException, IOException {
+    public ShortUrl createUrlObject(@RequestBody URL myNewUrl) throws MalformedURLException, IOException {
         ShortUrl objectToCreate = new ShortUrl();
         objectToCreate.setId(UUID.randomUUID().toString());
         objectToCreate.setShortId(shortIdGenerator.getThisShortID());
         objectToCreate.setRealUrl(new URL(myNewUrl.toString()));
-        this.savefile.createFile(new File("C:\\Users\\9101015H\\www"), "fileTest",objectToCreate);
+        this.savefile.createFile(new File("C:\\Users\\9101015H\\www"), "fileTest", objectToCreate);
         return objectToCreate;
     }
 
@@ -43,14 +42,14 @@ public class ShortUrlController {
         ObjectMapper mapper = new ObjectMapper();
         mapper.enable(SerializationFeature.INDENT_OUTPUT);
         try {
-            ShortUrl[] shortsUrl = mapper.readValue(new File("C:\\Users\\7902872D\\www\\fileTest.json"), ShortUrl[].class);
+            ShortUrl[] shortsUrl = mapper.readValue(new File("C:\\Users\\9101015H\\www\\fileTest.json"), ShortUrl[].class);
             for (ShortUrl shortUrl : shortsUrl) {
                 if (shortId.equals(shortUrl.getShortId())) {
                     String realUrl = shortUrl.getRealUrl().toString();
                     return new ModelAndView("redirect:" + realUrl);
                 }
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
         return new ModelAndView("redirect://www.google.com");
